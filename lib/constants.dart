57847c639b9f1a43da30a37d4bc33608aad9e6d6
sam <bellosamuelb@gmail.com>
2021-11-27 10:55:09 +0800

    Resistration sample POST from flutter

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+double pToF(double double) => double/1.5;
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pahiream_frontend/login/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(appBar: AppBar(), body: Container(color: Colors.amber,),)
+      home: MyLoginPage()
     );
   }
 }
