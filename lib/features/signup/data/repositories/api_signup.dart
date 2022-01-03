441ed4af57c54def51215f8c8fbde149d9c8a0e2
rlysam <bellosamuelb@gmail.com>
2022-01-03 13:29:19 +0800

    New Structure For Flutter. All working.

--- lib/features/signup/data/repositories/api_signup.dart
+++ lib/features/signup/data/repositories/api_signup.dart
@@ -1,4 +1,3 @@
-// import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:pahiream_frontend/features/signup/data/models/user.dart';
 import 'dart:convert';
