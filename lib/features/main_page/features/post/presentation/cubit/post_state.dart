8869b846438ff81ca0a1d08c3ff4ed79a929752b
rlysam <bellosamuelb@gmail.com>
2022-01-04 22:53:30 +0800

    Added Feature Notes for lib and main_page. | Added post feature to main_page

---
+++ lib/features/main_page/features/post/presentation/cubit/post_state.dart
@@ -0,0 +1,10 @@
+part of 'post_cubit.dart';
+
+abstract class PostState extends Equatable {
+  const PostState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class PostInitial extends PostState {}
