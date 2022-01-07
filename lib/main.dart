396c3701795e31bed7c75a240a17c70791000d99
rlysam <bellosamuelb@gmail.com>
2022-01-07 12:05:39 +0800

    Working switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:pahiream_frontend/features/main_page/features/post/data/datasources/post_api.dart';
-import 'package:pahiream_frontend/features/main_page/features/post/data/models/pasabay.dart';
-import 'package:pahiream_frontend/features/main_page/features/post/data/repositories/post_repository.dart';
-import 'package:pahiream_frontend/features/main_page/features/post/presentation/cubit/post_cubit.dart';
-import 'package:pahiream_frontend/features/main_page/features/post/presentation/widgets/post_widget.dart';
+
 import 'package:pahiream_frontend/route_generator.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
-//   void displayData() async {
-//       final PostRepository _postRepository;
-//       final pasabayData = await _postRepository.fetchPasabayPost(1);
-//     print('\n\nHELLO FROM MAIN\n\n');
-//     print(pasabayData);
-//     print(pasabayData.runtimeType);
-//   }
+  bool isPasabay = true;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    var sam = requestPasabayPostByID(1);
-    print('\n\nDito sa main');
-    print(sam.then((value) => print(value)));
-    print(sam.runtimeType);
-    return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'PahireAm v1.0',
-      theme: ThemeData(
-        //Nunito
-        textTheme: GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
-        primarySwatch: Colors.blue,
-      ),
-      //   initialRoute: '/',
-      //   onGenerateRoute: RouteGenerator.generateRoute,
-      home: Scaffold(
-          body: Center(
-              child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Doing POST in general'),
-          Text('PRINT LAMAN NG API RESPONSE'),
-          PostWidget(),
-        ],
-      ))),
-    );
+    print(widget.isPasabay);
+    return Provider<bool>(
+        create: (context) => widget.isPasabay,
+        child: MaterialApp(
+          debugShowCheckedModeBanner: widget.isPasabay,
+          title: 'PahireAm v1.0',
+          theme: ThemeData(
+            textTheme: GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
+            primarySwatch: Colors.blue,
+          ),
+          initialRoute: '/',
+          onGenerateRoute: RouteGenerator.generateRoute,
+        ));
   }
 }
